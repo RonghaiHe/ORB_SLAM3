aef460bc2dadb7ef3cfddf1a9366339efcfcda54
RonghaiHe <hrhkjys@qq.com>
2023-01-09 16:52:43 +0800

    Add multi-session shell

---
+++ run_multi_session_EuRoC.sh
@@ -0,0 +1,9 @@
+cd /data/herh/ORB_SLAM3/EuRoC/
+
+/home/herh/ORB_SLAM3/Examples/Monocular/mono_euroc  /home/herh/ORB_SLAM3/Vocabulary/ORBvoc.txt /home/herh/ORB_SLAM3/Examples/Monocular/EuRoC.yaml ./V2_01_easy /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V201.txt ./V2_02_medium /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V202.txt ./V2_03_difficult /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V203.txt ./V2_123/Mono/
+
+/home/herh/ORB_SLAM3/Examples/Monocular-Inertial/mono_inertial_euroc  /home/herh/ORB_SLAM3/Vocabulary/ORBvoc.txt /home/herh/ORB_SLAM3/Examples/Monocular-Inertial/EuRoC.yaml ./V2_01_easy /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V201.txt ./V2_02_medium /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V202.txt ./V2_03_difficult /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V203.txt ./V2_123/MonoInertial/
+
+/home/herh/ORB_SLAM3/Examples/Stereo/stereo_euroc  /home/herh/ORB_SLAM3/Vocabulary/ORBvoc.txt /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC.yaml ./V2_01_easy /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V201.txt ./V2_02_medium /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V202.txt ./V2_03_difficult /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V203.txt ./V2_123/Stereo/
+
+/home/herh/ORB_SLAM3/Examples/Stereo-Inertial/stereo_inertial_euroc  /home/herh/ORB_SLAM3/Vocabulary/ORBvoc.txt /home/herh/ORB_SLAM3/Examples/Stereo-Inertial/EuRoC.yaml ./V2_01_easy /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V201.txt ./V2_02_medium /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V202.txt ./V2_03_difficult /home/herh/ORB_SLAM3/Examples/Stereo/EuRoC_TimeStamps/V203.txt ./V2_123/StereoInertial/

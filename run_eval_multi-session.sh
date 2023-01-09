9a90a79fc2850c84b4283c2ead00aab6ee604835
RonghaiHe <hrhkjys@qq.com>
2023-01-09 17:42:41 +0800

    Precise multi eva sh

--- run_eval_multi-session.sh
+++ run_eval_multi-session.sh
@@ -6,7 +6,7 @@ camera_list=(Mono MonoInertial Stereo StereoInertial)
 
 for j in ${camera_list[*]}
 do
-        python2 /home/herh/ORB_SLAM3/evaluation/evaluate_ate_scale.py "../V1_01_easy/mav0/state_groundtruth_estimate0/data.csv ../V1_02_medium/mav0/state_groundtruth_estimate0/data.csv ../V1_03_difficult/mav0/state_groundtruth_estimate0/data.csv" "./${j}/f_.txt" --plot "./${j}/compare_f" --verbose --need_scale --save_rmse "./${j}/info_f.txt"
-        python2 /home/herh/ORB_SLAM3/evaluation/evaluate_ate_scale.py "../V1_01_easy/mav0/state_groundtruth_estimate0/data.csv ../V1_02_medium/mav0/state_groundtruth_estimate0/data.csv ../V1_03_difficult/mav0/state_groundtruth_estimate0/data.csv" "./${j}/kf_.txt" --plot "./${j}/compare_kf" --verbose --need_scale --save_rmse "./${j}/info_kf.txt"
+        python2 /home/herh/ORB_SLAM3/evaluation/evaluate_ate_scale.py "../V2_01_easy/mav0/state_groundtruth_estimate0/data.csv ../V2_02_medium/mav0/state_groundtruth_estimate0/data.csv ../V2_03_difficult/mav0/state_groundtruth_estimate0/data.csv" "./${j}/f_.txt" --plot "./${j}/compare_f" --verbose --need_scale --save_rmse "./${j}/info_f.txt"
+        python2 /home/herh/ORB_SLAM3/evaluation/evaluate_ate_scale.py "../V2_01_easy/mav0/state_groundtruth_estimate0/data.csv ../V2_02_medium/mav0/state_groundtruth_estimate0/data.csv ../V2_03_difficult/mav0/state_groundtruth_estimate0/data.csv" "./${j}/kf_.txt" --plot "./${j}/compare_kf" --verbose --need_scale --save_rmse "./${j}/info_kf.txt"
         done
 done
